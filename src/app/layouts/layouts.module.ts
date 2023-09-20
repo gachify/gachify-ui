2853ff69041ab63e08356e2ad17434e26814d484
Vladislav Shukelait <mrcatlait@gmail.com>
2023-09-20 14:32:15 +0300

    feat(ui): add basic nav drawer layout

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { MatIconModule } from '@angular/material/icon'
 
 import { BlankLayoutComponent, DefaultLayoutComponent } from './components'
 
+import { SharedModule } from '@shared/shared.module'
+
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, MatIconModule, RouterModule, SharedModule],
   declarations: [BlankLayoutComponent, DefaultLayoutComponent],
   exports: [BlankLayoutComponent, DefaultLayoutComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import { NgLetDirective, DataCyDirective } from './directives'
+import { NgLetDirective, DataCyDirective, MatIconOutlinedDirective } from './directives'
 import { DurationPipe } from './pipes'
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [DataCyDirective, DurationPipe, NgLetDirective],
-  exports: [CommonModule, DataCyDirective, DurationPipe, FormsModule, NgLetDirective, ReactiveFormsModule],
+  declarations: [DataCyDirective, DurationPipe, MatIconOutlinedDirective, NgLetDirective],
+  exports: [
+    CommonModule,
+    DataCyDirective,
+    DurationPipe,
+    FormsModule,
+    MatIconOutlinedDirective,
+    NgLetDirective,
+    ReactiveFormsModule,
+  ],
 })
 export class SharedModule {}
