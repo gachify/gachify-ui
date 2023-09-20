190723a7b87b34dde37a8cf1473b8a100532788c
Vladislav Shukelait <mrcatlait@gmail.com>
2023-09-20 12:34:34 +0300

    feat(ui): add headless slider styling

--- src/app/pages/home/home-page.module.ts
+++ src/app/pages/home/home-page.module.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSliderModule } from '@angular/material/slider'
 
 import { HomePageComponent } from './home-page.component'
 import { HomePageRoutingModule } from './home-page-routing.module'
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [HomePageRoutingModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule],
+  imports: [HomePageRoutingModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSliderModule],
 })
 export class HomePageModule {}
