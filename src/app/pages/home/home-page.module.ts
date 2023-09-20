2eebfc2d6ec5a187ac0a6c60c5aa572ef7f30a5a
Vladislav Shukelait <mrcatlait@gmail.com>
2023-09-20 12:13:43 +0300

    feat(ui): added styling for headless input fields

--- src/app/pages/home/home-page.module.ts
+++ src/app/pages/home/home-page.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
-import { MatRippleModule } from '@angular/material/core'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 import { HomePageComponent } from './home-page.component'
 import { HomePageRoutingModule } from './home-page-routing.module'
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [HomePageRoutingModule, MatButtonModule, MatRippleModule],
+  imports: [HomePageRoutingModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule],
 })
 export class HomePageModule {}
